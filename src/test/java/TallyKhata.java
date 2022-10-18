fc5b9dfabb5a4b3abdc26b67a0f8a58f90426905
A.B.M.Shihab Uddin <a.b.m.shihab57@gmail.com>
2022-10-18 21:55:52 +0600

    static login function for dynamic token value

--- src/test/java/TallyKhata.java
+++ src/test/java/TallyKhata.java
@@ -13,11 +13,9 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class TallyKhata {
 
-    String token;
     long requestId = (long) Math.floor(Math.random() * 9_000_000_000L) + 1_000_000_000L;
 
-    @Test(priority = 0)
-    void test_login() throws ParseException {
+    static String login() throws ParseException {
         JSONObject requestBody = new JSONObject();
 
         requestBody.put("app_type", "TALLYKHATA");
@@ -34,21 +32,22 @@ public class TallyKhata {
                 header("x-device-id", "30c3f35c-d6b2-47b4-ba86-b81eea6af4a2").
                 body(requestBody.toJSONString()).
                 when().
-                    post("https://stgqa.tallykhata.com/wallet/api/tp-proxy/user/login");
+                post("https://stgqa.tallykhata.com/wallet/api/tp-proxy/user/login");
 
         String loginResponse = response.getBody().asString();
         JSONParser parser = new JSONParser();
         JSONObject obj = (JSONObject) parser.parse(loginResponse);
 
-        token = "token " + (String) obj.get("token");
+        String token = "token " + (String) obj.get("token");
         System.out.println("response is: " + token);
+        return token;
     }
 
-    @Test (enabled = false, priority = 1)
-    void test_loan_repayment() {
+    @Test (enabled = true, priority = 1)
+    void test_loan_repayment() throws ParseException {
         JSONObject requestBody = new JSONObject();
 
-        requestBody.put("amount", 10);
+        requestBody.put("amount", 21);
         requestBody.put("credential", "9316");
         requestBody.put("location", null);
         requestBody.put("receiver", "460196000923");
@@ -64,7 +63,7 @@ public class TallyKhata {
 
         given().
                 header("content-type", "application/json").
-                header("Authorization", token).
+                header("Authorization", login()).
                 header("x-request-id", "1c2ea1a42bcd5c93:TK_ANDROID:15458807928059").
                 header("np-uuid", "0069a6b1-b510-443b-b51b-db84889ef9f6").
                 header("np-userref", "01621215877").
@@ -78,8 +77,8 @@ public class TallyKhata {
                 log().all();
     }
 
-    @Test (enabled = false, priority = 2)
-    void test_recharge() {
+    @Test (enabled = true, priority = 2)
+    void test_recharge() throws ParseException {
         JSONObject requestBody = new JSONObject();
         requestBody.put("amount", 11);
         requestBody.put("credential", "9316");
@@ -95,7 +94,7 @@ public class TallyKhata {
 
         given().
                 header("content-type", "application/json").
-                header("Authorization", token).
+                header("Authorization", login()).
                 body(requestBody.toJSONString()).
         when().
                 post("https://stgnpapigw.nobopay.com/api/v1/transaction/recharge-mobile").
@@ -105,7 +104,7 @@ public class TallyKhata {
     }
 
     @Test (enabled = true, priority = 3)
-    void test_loan_payment() {
+    void test_loan_payment() throws ParseException {
         JSONObject requestBody = new JSONObject();
 
         requestBody.put("amount", 10);
@@ -124,7 +123,7 @@ public class TallyKhata {
 
         given().
                 header("content-type", "application/json").
-                header("Authorization", token).
+                header("Authorization", login()).
                 body(requestBody.toJSONString()).
         when().
                 post("https://stgnpapigw.nobopay.com/api/v1/transaction/tk/payment/with/loan").
@@ -134,7 +133,7 @@ public class TallyKhata {
     }
 
     @Test (enabled = true, priority = 4)
-    void test_send_money() {
+    void test_send_money() throws ParseException {
         JSONObject requestBody = new JSONObject();
 
         requestBody.put("amount", 10);
@@ -152,7 +151,7 @@ public class TallyKhata {
 
         given().
                 header("content-type", "application/json").
-                header("Authorization", token).
+                header("Authorization", login()).
                 body(requestBody.toJSONString()).
         when().
                 post("https://stgnpapigw.nobopay.com/api/v1/transaction/tk/send-money").
